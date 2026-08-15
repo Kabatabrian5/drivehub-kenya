55041bf0e6eccac2ee1bd97047a2ef2020375ded
Brian kabata <kabatabrian5@gmail.com>
2026-08-15 11:01:31 +0300

    Create cars-data.js

---
+++ cars-data.js
@@ -0,0 +1,42 @@
+const cars = [
+    {
+        make: "Toyota",
+        model: "RAV4",
+        price: 3200000,
+        year: 2017,
+        location: "Nairobi",
+        transmission: "Automatic",
+        fuel: "Petrol",
+        cc: 2000
+    },
+    {
+        make: "Mazda",
+        model: "Demio",
+        price: 950000,
+        year: 2016,
+        location: "Mombasa",
+        transmission: "Automatic",
+        fuel: "Petrol",
+        cc: 1300
+    },
+    {
+        make: "Subaru",
+        model: "Forester",
+        price: 2800000,
+        year: 2015,
+        location: "Nakuru",
+        transmission: "Automatic",
+        fuel: "Petrol",
+        cc: 2500
+    },
+    {
+        make: "Toyota",
+        model: "Fielder",
+        price: 1650000,
+        year: 2018,
+        location: "Nairobi",
+        transmission: "Automatic",
+        fuel: "Hybrid",
+        cc: 1500
+    }
+];
